04fcbb389c72a95e7ee1d228a6504363708b866e
Tiago Lage Payne de Pádua <tiagolpadua@gmail.com>
2023-05-21 15:44:00 -0300

    ajusta autenticacao

--- src/routes/categoriesRoutes.js
+++ src/routes/categoriesRoutes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 
 import CategoryController from '../controllers/CategoryController.js';
+import middlewaresAutenticacao from '../middlewares/autenticacao.js';
 
 const router = express.Router();
 
 router
   .get('/api/categories', CategoryController.find)
   .get('/api/categories/:id', CategoryController.findById)
-  .post('/api/admin/categories', CategoryController.save)
-  .put('/api/categories/:id/activate', CategoryController.activate)
-  .put('/api/admin/categories/:id', CategoryController.update)
-  .delete('/api/admin/categories/:id', CategoryController.delete);
+  .post('/api/admin/categories', middlewaresAutenticacao.bearer, CategoryController.save)
+  .put('/api/categories/:id/activate', middlewaresAutenticacao.bearer, CategoryController.activate)
+  .put('/api/admin/categories/:id', middlewaresAutenticacao.bearer, CategoryController.update)
+  .delete('/api/admin/categories/:id', middlewaresAutenticacao.bearer, CategoryController.delete);
 
 export default router;
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import middlewaresAutenticacao from '../middlewares/autenticacao.js';
 
 import ProductController from '../controllers/ProductController.js';
 
@@ -7,8 +8,8 @@ const router = express.Router();
 router
   .get('/api/products', ProductController.find)
   .get('/api/products/:id', ProductController.findById)
-  .post('/api/admin/products', ProductController.save)
-  .put('/api/admin/products/:id', ProductController.update)
-  .delete('/api/admin/products/:id', ProductController.delete);
+  .post('/api/admin/products', middlewaresAutenticacao.bearer, ProductController.save)
+  .put('/api/admin/products/:id', middlewaresAutenticacao.bearer, ProductController.update)
+  .delete('/api/admin/products/:id', middlewaresAutenticacao.bearer, ProductController.delete);
 
 export default router;
